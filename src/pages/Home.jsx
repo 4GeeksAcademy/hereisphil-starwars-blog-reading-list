59c9c21e62acff6fafcada6d32c0d47e37b7f273
Phillip Cantu <thereisphil@gmail.com>
2025-10-01 23:46:33 +0000

    Removed characters page. Made 3 useEffects to fetch people, planets, and vehicles within the Home.jsx. Added to store via dispatch and mapped 3 different <ul>'s for testing.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,27 +2,65 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { useEffect } from "react";
 
 export const Home = () => {
+  const { store, dispatch } = useGlobalReducer();
+  const BASE_URL = "https://www.swapi.tech/api/";
 
-const BASE_URL = "https://www.swapi.tech/api/";
+  // FETCH THE CHARACTERS
   useEffect(() => {
-    async function fetchPeople() {
+    async function fetchCharacters() {
       try {
-		const response = await fetch(`${BASE_URL}people`);
-        console.log("response:", response);
+        const response = await fetch(`${BASE_URL}people`);
         const data = await response.json();
-        console.log("data json:", data);
-        const peopleData = data.results;
-        console.log("peopleData array:", peopleData);
-        // setUser(userData);
+        const characterData = data.results;
+        dispatch({ type: "set_characters", payload: characterData });
       } catch (error) {
         console.log("There was an error:", error);
       }
     }
-    fetchPeople();
+    fetchCharacters();
   }, []);
 
-	return (
-		<div className="text-center mt-5">
-		</div>
-	);
-}; 
+  // FETCH PLANETS
+  useEffect(() => {
+    async function fetchPlanets() {
+      try {
+        const response = await fetch(`${BASE_URL}planets`);
+        const data = await response.json();
+        const planetsData = data.results;
+        dispatch({ type: "set_planets", payload: planetsData });
+      } catch (error) {
+        console.log("There was an error:", error);
+      }
+    }
+    fetchPlanets();
+  }, []);
+
+  // FETCH VEHICLES
+  useEffect(() => {
+    async function fetchVehicles() {
+      try {
+        const response = await fetch(`${BASE_URL}vehicles`);
+        const data = await response.json();
+        const vehiclesData = data.results;
+        dispatch({ type: "set_vehicles", payload: vehiclesData });
+      } catch (error) {
+        console.log("There was an error:", error);
+      }
+    }
+    fetchVehicles();
+  }, []);
+
+  return (
+    <main>
+      <ul>
+        {store.characters.map((character) => <li key={`${character.uid} ${character.name}`}>{character.name}</li>)}
+      </ul>
+      <ul>
+        {store.planets.map((planet) => <li key={`${planet.uid} ${planet.name}`}>{planet.name}</li>)}
+      </ul>
+      <ul>
+        {store.vehicles.map((vehicle) => <li key={`${vehicle.uid} ${vehicle.name}`}>{vehicle.name}</li>)}
+      </ul>
+    </main>
+  );
+};
--- src/store.js
+++ src/store.js
@@ -1,16 +1,25 @@
 export const initialStore = () => {
   return {
-    people: [],
+    characters: [],
     vehicles: [],
     planets: [],
+    favorites: [],
   }
 }
 
 export default function storeReducer(store, action = {}) {
   switch (action.type) {
-    case "set_people":
+    case "set_characters":
       return {...store,
-        people: action.payload
+        characters: action.payload
+      };
+      case "set_planets":
+      return {...store,
+        planets: action.payload
+      };
+      case "set_vehicles":
+      return {...store,
+        vehicles: action.payload
       };
     default:
       throw Error('Unknown action.');
