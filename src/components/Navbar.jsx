46122f5aa4b91c24b3dfd9cfd7e48b23d5b50b98
Phillip Cantu <thereisphil@gmail.com>
2025-10-01 02:07:48 +0000

    Setup Chracters.jsx page with Fetch of people

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,12 +6,17 @@ export const Navbar = () => {
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">Starwars Blog</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+					<div className="dropdown">
+						<button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Favorites
+						</button>
+						<ul className="dropdown-menu">
+							<li className="px-2 d-flex justify-content-between">Luke Skywalker <span><i className="fa-solid fa-trash-can"></i></span></li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,28 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { useEffect } from "react";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+const BASE_URL = "https://www.swapi.tech/api/";
+  useEffect(() => {
+    async function fetchPeople() {
+      try {
+		const response = await fetch(`${BASE_URL}people`);
+        console.log("response:", response);
+        const data = await response.json();
+        console.log("data json:", data);
+        const peopleData = data.results;
+        console.log("peopleData array:", peopleData);
+        // setUser(userData);
+      } catch (error) {
+        console.log("There was an error:", error);
+      }
+    }
+    fetchPeople();
+  }, []);
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
 		</div>
 	);
 }; 
